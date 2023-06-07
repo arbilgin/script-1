50a40ece880928d5f0303a89750241c56598f55d
arbilgin <arda@mentholprotocol.com>
2023-06-07 21:58:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+contractABI = [{ "inputs": [], "name": "count", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "dec", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "get", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "inc", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+const provider = new ethers.providers.AlchemyProvider('goerli', process.env.TEST_API_KEY);
+const wallet = new ethers.Wallet(process.env.TEST_PRIVATE_KEY, provider);
+
+async function main() {
+
+    const counterContract = new ethers.Contract(
+        '0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A',
+        contractABI,
+        wallet
+    );
+
+    let increment = await counterContract.inc();
+
+    console.log(increment.hash);
+
+    const currentCounterValue = await counterContract.get();
+    
+    console.log(currentCounterValue.toString());
+
+
+}
+
+main();
